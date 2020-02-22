7c2c3f7adfc200d5d8fd7682ec7434911abbbbfc
Osmar Leandro <osmarleandrods@gmail.com>
2020-02-21 21:32:37 -0300

    [ref] Push down method from  SuperclassExtracted to JsonContentAggregator#onMessage()

--- spqr-operators/spqr-json/src/main/java/com/ottogroup/bi/spqr/operator/json/aggregator/SuperclassExtracted.java
+++ spqr-operators/spqr-json/src/main/java/com/ottogroup/bi/spqr/operator/json/aggregator/SuperclassExtracted.java
@@ -1,20 +1,11 @@
 package com.ottogroup.bi.spqr.operator.json.aggregator;
 
 import com.ottogroup.bi.spqr.pipeline.component.operator.DelayedResponseOperator;
-import com.ottogroup.bi.spqr.pipeline.message.StreamingDataMessage;
 
 public class SuperclassExtracted {
 
 	public SuperclassExtracted() {
 		super();
-	}
-
-	/**
-	 * @see com.ottogroup.bi.spqr.pipeline.component.operator.DelayedResponseOperator#onMessage(com.ottogroup.bi.spqr.pipeline.message.StreamingDataMessage)
-	 * @deprecated Use {@link com.ottogroup.bi.spqr.pipeline.message.StreamingDataMessage#onMessage(com.ottogroup.bi.spqr.operator.json.aggregator.JsonContentAggregator)} instead
-	 */
-	public void onMessage(StreamingDataMessage message) {
-		message.onMessage(this);
 	}
 
 }
