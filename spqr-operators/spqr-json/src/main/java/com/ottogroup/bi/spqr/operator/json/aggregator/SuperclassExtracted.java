bbd9137cd7461098723187d9d44242b376a9238b
Osmar Leandro <osmarleandrods@gmail.com>
2020-02-21 21:45:12 -0300

    [ref] Pull up method from JsonContentAggregator#getTextFieldValue() to SuperclassExtracted

--- spqr-operators/spqr-json/src/main/java/com/ottogroup/bi/spqr/operator/json/aggregator/SuperclassExtracted.java
+++ spqr-operators/spqr-json/src/main/java/com/ottogroup/bi/spqr/operator/json/aggregator/SuperclassExtracted.java
@@ -1,11 +1,30 @@
 package com.ottogroup.bi.spqr.operator.json.aggregator;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.ottogroup.bi.spqr.pipeline.component.operator.DelayedResponseOperator;
 
 public class SuperclassExtracted {
 
 	public SuperclassExtracted() {
 		super();
+	}
+
+	/**
+	 * Walks along the path provided and reads out the leaf value which is returned as string 
+	 * @param jsonNode
+	 * @param fieldPath
+	 * @return
+	 */
+	public String getTextFieldValue(final JsonNode jsonNode, final String[] fieldPath) {
+	
+		int fieldAccessStep = 0;
+		JsonNode contentNode = jsonNode;
+		while(fieldAccessStep < fieldPath.length) {
+			contentNode = contentNode.get(fieldPath[fieldAccessStep]);
+			fieldAccessStep++;
+		}	
+	
+		return contentNode.textValue();
 	}
 
 }
